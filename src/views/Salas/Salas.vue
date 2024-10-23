141004c0478ca6dc69ee2267c419dc4982a240c2
DEFAPROGRAM <defigueroa01@gmail.com>
2024-10-23 16:47:05 -0500

    ACTUALIZACIONES 2

---
+++ src/views/Salas/Salas.vue
@@ -0,0 +1,36 @@
+<template>
+  <LayoutMain>
+    <template #slotLayout>
+      <Header 
+      :titulo="'Salas'"
+      :tituloBoton="'Crear Salas'"
+      >
+      </Header>
+      <!-- componente/ -->
+      <Formulario :titulo="'titulo del formulario'">
+
+        <template #slotForm>
+          <formSalas/>
+        </template>
+
+      </Formulario>
+
+          
+    </template>
+
+
+    </LayoutMain>
+  </template>
+    
+  
+  <script lang="ts" setup>
+  import { reactive, ref } from 'vue'
+  import LayoutMain from '../../components/LayoutMain.vue';
+  import  formSalas from './components/formSalas.vue'
+  import Formulario from '../../components/Formulario.vue';
+  import Header from '../../components/Header.vue';
+  import  {Delete,Edit} from "@element-plus/icons-vue"
+
+
+  
+  </script>
---
+++ src/views/Sedes/Sedes.vue
@@ -0,0 +1,37 @@
+<template>
+  <LayoutMain>
+    <template #slotLayout>
+      <Header 
+      :titulo="'Sedes'"
+      :tituloBoton="'Crear Sedes'"
+      >
+      </Header>
+      <!-- componente/ -->
+      <Formulario :titulo="'titulo del formulario'">
+
+        <template #slotForm>
+          <formSedes/>
+        </template>
+
+      </Formulario>
+
+          
+         
+    </template>
+
+
+    </LayoutMain>
+  </template>
+    
+  
+  <script lang="ts" setup>
+  import { reactive, ref } from 'vue'
+  import LayoutMain from '../../components/LayoutMain.vue';
+  import  formSedes from './components/formSedes.vue'
+  import Formulario from '../../components/Formulario.vue';
+  import Header from '../../components/Header.vue';
+  import  {Delete,Edit} from "@element-plus/icons-vue"
+
+
+  
+  </script>
